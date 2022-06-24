de0c4d57c0bfe359be20b506c3e4cb5823c216a5
Nuriddin70 <nuriddin3050@gmail.com>
2022-06-25 00:01:30 +0900

    jarayon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,10 @@
 import React, {useState} from 'react'
-<<<<<<< HEAD
 import {FaBars, FaTimes,FaGithub, FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from "../assets/logo.png"
 
 
-=======
-import {FaBars, FaTimes} from 'react-icons/fa'
-import Logo from "../assets/logo.png"
-
->>>>>>> 780d1f2a205e74a0b4af60888e0c843152351d44
 const Navbar = () => {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
@@ -18,14 +12,13 @@ const Navbar = () => {
 
   return (
     <div className=' fixed w-full h-[5rem] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      <div>
-<<<<<<< HEAD
+      <div><<<<<<< HEAD
         <input type='image' src={Logo} alt="Logo Image" style={{width: '3.125rem'}} />
 =======
         <image src={Logo} alt="Logo Image" style={{width: '3.125rem'}} />
 >>>>>>> 780d1f2a205e74a0b4af60888e0c843152351d44
-      </div>
 
+      </div>
       {/* menu */}
       
         <ul className='hidden md:flex '>
@@ -79,7 +72,6 @@ const Navbar = () => {
     </div>
 =======
     <div className='hidden'> </div>
->>>>>>> 780d1f2a205e74a0b4af60888e0c843152351d44
     </div>
   )
 }
