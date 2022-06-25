fcac6c7320e0076dd5ee0fc9a12abd6cfa1e32e3
Nuriddin70 <nuriddin3050@gmail.com>
2022-06-25 11:59:21 +0900

    fixed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {HiArrowNarrowRight} from  'react-icons/hi'
 
-const Hero = () => {
-  return (
+const Hero = () =>{
+return (
     <div name='hero' className='w-full h-screen bg-[#0a192f]'>
       
       {/* Conatiner */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <div className='max-w-[950px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is </p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Nuriddin</h1>
         <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>I am a Front-End Developer.</h2>
