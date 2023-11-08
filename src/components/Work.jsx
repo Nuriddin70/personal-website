7256e53cc621e832c76afd36e216310732f3c52d
Nuriddin70 <nuriddin3050@gmail.com>
2023-11-08 20:13:47 +0900

    added some projects to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
-import ComingSoon from '../assets/comingSoon.jpg'
+import nnHealth from '../assets/nn-health.png'
+import organica from '../assets/organica.png'
+import centralMovie from '../assets/centralMovie.png'
+import weatherApp from '../assets/weatherApp.png'
+import todoList from '../assets/todoList.png'
+import cryptoBase from '../assets/cryptoBase.png'
+import shop from '../assets/shop.png'
+import betsEats from '../assets/betsEats.png'
+
+
+
 
 
 
@@ -16,43 +26,21 @@ const Work = () => {
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 '>
           
           {/* Grid Item */} 
-          <div style={{ backgroundImage: `url(${ComingSoon})` }}
+          <div style={{ backgroundImage: `url(${nnHealth})` }}
           className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
           
             {/* Hover Effects */} 
             <div className='opacity-0 group-hover:opacity-100 text-center'>
               <span className='text-2xl font-bold text-white tracking-wider '>
-              Coming Soon Application
+              Virtual healthcare
               </span>
               <div className='pt-8 text-center'>
-                <a href="/">
+                <a href="https://github.com/Nuriddin70/Health-Css.git">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div style={{ backgroundImage: `url(${ComingSoon})` }}
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
-          
-            {/* Hover Effects */} 
-            <div className='opacity-0 group-hover:opacity-100 text-center'>
-              <span className='text-2xl font-bold text-white tracking-wider '>
-              Coming Soon Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-                <a href="/">
+                <a href=" https://nn-health.netlify.app/">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
@@ -61,21 +49,44 @@ const Work = () => {
             </div>
           </div>
 
-          <div style={{ backgroundImage: `url(${ComingSoon})` }}
+          <div style={{ backgroundImage: `url(${organica})` }}
           className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
           
             {/* Hover Effects */} 
             <div className='opacity-0 group-hover:opacity-100 text-center'>
               <span className='text-2xl font-bold text-white tracking-wider '>
-              Coming Soon Application
+              Organica vegetables
               </span>
               <div className='pt-8 text-center'>
-                <a href="/">
+                <a href="https://github.com/Nuriddin70/OrganicIca-vanillaJS.git">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://idyllic-puppy-85a047.netlify.app/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div style={{ backgroundImage: `url(${centralMovie})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
+          
+            {/* Hover Effects */} 
+            <div className='opacity-0 group-hover:opacity-100 text-center'>
+              <span className='text-2xl font-bold text-white tracking-wider '>
+              Central Movie
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://github.com/Nuriddin70/javaScript-project.git">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+                <a href="https://nn-movie.netlify.app/">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
@@ -85,44 +96,21 @@ const Work = () => {
           </div>
 
           {/* Grid Item */} 
-          <div style={{ backgroundImage: `url(${ComingSoon})` }}
+          <div style={{ backgroundImage: `url(${weatherApp})` }}
           className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
           
             {/* Hover Effects */} 
             <div className='opacity-0 group-hover:opacity-100 text-center'>
               <span className='text-2xl font-bold text-white tracking-wider '>
-              Coming Soon Application
+              Weather App
               </span>
               <div className='pt-8 text-center'>
-                <a href="/">
+                <a href="https://github.com/Nuriddin70/Weather-App.git">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
- 
-          <div style={{ backgroundImage: `url(${ComingSoon})` }}
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
-          
-            {/* Hover Effects */} 
-            <div className='opacity-0 group-hover:opacity-100 text-center'>
-              <span className='text-2xl font-bold text-white tracking-wider '>
-              Coming Soon Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-                <a href="/">
+                <a href="https://luxury-custard-3a706f.netlify.app/">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
@@ -131,27 +119,99 @@ const Work = () => {
             </div>
           </div>
 
-          <div style={{ backgroundImage: `url(${ComingSoon})` }}
+          <div style={{ backgroundImage: `url(${todoList})` }}
           className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
           
             {/* Hover Effects */} 
             <div className='opacity-0 group-hover:opacity-100 text-center'>
               <span className='text-2xl font-bold text-white tracking-wider '>
-              Coming Soon Application
+              Todo List
               </span>
               <div className='pt-8 text-center'>
-                <a href="/">
+                <a href="https://github.com/Nuriddin70/anotherTodoListJs.git">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://nn-crud-todo.netlify.app/">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
               </div>
             </div>
+          </div>
+
+          <div style={{ backgroundImage: `url(${cryptoBase})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
+          
+            {/* Hover Effects */} 
+            <div className='opacity-0 group-hover:opacity-100 text-center'>
+              <span className='text-2xl font-bold text-white tracking-wider '>
+              Crypto Base
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://github.com/Nuriddin70/Cripto-base.git">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+                <a href="https://nn-criptobase.netlify.app/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          <div style={{ backgroundImage: `url(${shop})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
+          
+            {/* Hover Effects */} 
+            <div className='opacity-0 group-hover:opacity-100 text-center'>
+              <span className='text-2xl font-bold text-white tracking-wider '>
+              Shop
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://github.com/Nuriddin70/Nur-card.git">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+                <a href="https://nur-shop.netlify.app/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+            
+          </div>
+
+          <div style={{ backgroundImage: `url(${betsEats})` }}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' >
+          
+            {/* Hover Effects */} 
+            <div className='opacity-0 group-hover:opacity-100 text-center'>
+              <span className='text-2xl font-bold text-white tracking-wider '>
+              Best Eats
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://github.com/Nuriddin70/besr-eats.git">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+                <a href="https://nur-eats.netlify.app/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+
           </div>
 
         </div>
